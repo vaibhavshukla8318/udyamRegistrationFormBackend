9ed2b17348b3516dcd14be34cefc46c51a7e7c98
Vaibhav Shukla <vaibhavshukla264@gmail.com>
2025-08-13 20:07:23 +0530

    adding gitignore

--- middlewares/authRateLimiter.js
+++ middlewares/authRateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const otpLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 min
-  max: 5,
+  // windowMs: 5 * 60 * 1000, // 5 min
+  // max: 5,
   message: "Too many OTP requests from this IP, please try later."
 });
 
